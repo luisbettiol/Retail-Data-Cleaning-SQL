c1df03e8d4331f9bd9ce40d93465862b69713107
Luis Bettiol <luisbettiol18@gmail.com>
2026-04-29 20:53:04 -0400

    Update 02_Data_Modeling.sql

--- SQL_Scripts/02_Data_Modeling.sql
+++ SQL_Scripts/02_Data_Modeling.sql
@@ -9,24 +9,33 @@ en una Tabla de Hechos (Fact Table) vinculada a sus Dimensiones.
 */
 
 
+
 --       1. Definición de la Tabla de Hechos
+
 -- Inclusion de una Clave Primaria (PK) única para identificar cada registro transaccional.
+
 ALTER TABLE ventas_agr 
     ADD id_venta INT AUTO_INCREMENT PRIMARY KEY;
 
 
+
 --       2. Implementación de Integridad Referencial (Foreign Keys)
+
 -- Vincular la tabla de hechos con las dimensiones (Productos, Tiendas, Canales).
 -- Esto asegura que no existan ventas registradas de entidades inexistentes.
+
 ALTER TABLE ventas_agr
     ADD CONSTRAINT fk_ventas_productos FOREIGN KEY (id_prod) REFERENCES productos(id_prod),
     ADD CONSTRAINT fk_ventas_tiendas FOREIGN KEY (id_tienda) REFERENCES tiendas(id_tienda),
     ADD CONSTRAINT fk_ventas_canales FOREIGN KEY (id_canal) REFERENCES canales(id_canal);
 
 
+
 --       3. Creación de la Vista de Pedidos
+
 -- Generacion de un ID de Pedido dado que el dataset original no cuenta con uno. 
 -- Agrupación por Fecha, Tienda y Canal utilizando Window Functions.
+
 CREATE OR REPLACE VIEW v_ventas_agr_pedido AS
 WITH maestro_pedidos AS (
     SELECT 
@@ -53,8 +62,11 @@ JOIN maestro_pedidos mp
     AND v.id_canal = mp.id_canal;
 
 
+
  --     4. Validación del Modelo
+
  -- Verificamos que la cantidad de pedidos únicos (22,721) sea consistente con la lógica de negocio.
+
 SELECT 
    COUNT(DISTINCT id_pedido) AS total_pedidos_generados 
 FROM v_ventas_agr_pedido;
